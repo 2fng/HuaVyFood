3893b0bbf93a2f20aaebfc73d9ace6f072512861
Hua Son Tung <hua.son.tung@sun-asterisk.com>
2022-07-24 08:45:17 +0700

    First commit

---
+++ hua_vy_food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hua_vy_food
+//
+//  Created by Hua Son Tung on 24/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
